ac54eb1b994f0260e474f421bd3ff9f11eb3f75a
perfection <shark196@inbox.ru>
2023-04-12 09:47:00 +0300

    Решены 2 задачи по работе с классами.

--- Class_tasks/class_task_1.php
+++ Class_tasks/class_task_1.php
@@ -25,27 +25,3 @@ $animal_1->showMyAnimal();// Животное: Кошка Лакомство: Р
 $animal_2->showMyAnimal();// Дикое животное
 
 
-Class SquareChecker{
-    
-    public $part_list;
-
-    public function isSquare()
-    {
-        if (!isset($this->part_list)) {return 'Не указан массив со сторонами';}
-        if (count($this->part_list)!=4) {return 'Не корректное кол-во элементов фигуры';}
-
-        foreach ($this->part_list as $part){
-            if (!is_numeric($part)) { return 'Нужно вводить только числа';}
-            if ($part<0) {return 'Отрицательные цисла неприемлемы';}
-        }
-        
-        if (!(array_sum($this->part_list)/4==$this->part_list[0])){return 'Стороны квадрата должны быть равными';}
-
-        return '<div style="width:'.($this->part_list[0]).'px;height:'.($this->part_list[0]).'px;border:1px solid;"></div>';
-
-    }
-}
-
-$figure_1= new SquareChecker;
-$figure_1->part_list=[20,20,20,20];
-print $figure_1->isSquare();// Квадрат
