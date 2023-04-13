1fe6b193558d2a191782096a559d49038bf9744f
perfection <shark196@inbox.ru>
2023-04-13 16:39:16 +0300

    Внесение правок в задачи:
    - Все приведено к snake_case.
    - Добавлена проверка на факт наличия свойства $type в show_my_animal
    - Переработана проверка на совпадение всех елементов $part_list
    - Добавлены дополнительные проверки работы классов.

--- Class_tasks/class_task_1.php
+++ Class_tasks/class_task_1.php
@@ -1,27 +1,39 @@
 <?php
 
-Class Animal{
-    public $type;
-    public $feedObj;
+class Animal
+{
+    public string $type;
+    public string $feedObj;
 
-    public function showMyAnimal()
+    public function show_my_animal(): string
     {
-        if (isset($this->feedObj)){
-            return 'Животное: '.$this->type. ' Лакомство: '.$this->feedObj;
+        if (isset($this->type)) {
+            if (isset($this->feedObj)) {
+                return 'Животное: ' . $this->type . ' Лакомство: ' . $this->feedObj;
+            } else {
+                return 'Дикое животное';
+            }
         } else {
-            return 'Дикое животное';}
+            return 'Не указаны данные о животном!';
+        }
     }
-
 }
 
-$animal_1= new Animal;
-$animal_1->type='Кошка';
-$animal_1->feedObj='Рыба';
+$animal_1 = new Animal;
+$animal_1->type = 'Кошка';
+$animal_1->feedObj = 'Рыба';
 
-$animal_2= new Animal;
-$animal_2->type='Зебра';
+$animal_2 = new Animal;
+$animal_2->type = 'Зебра';
 
-$animal_1->showMyAnimal();// Животное: Кошка Лакомство: Рыба
-$animal_2->showMyAnimal();// Дикое животное
+$animal_3 = new Animal;
+$animal_3->type = 123;
+$animal_3->feedObj = 'Мыло';
 
+$animal_4 = new Animal;
+$animal_4->feedObj = 'Резина';
 
+echo $animal_1->show_my_animal(); // Животное: Кошка Лакомство: Рыба
+echo $animal_2->show_my_animal(); // Дикое животное
+echo $animal_3->show_my_animal(); // Животное: 123 Лакомство: Мыло (123=>'123')
+echo $animal_4->show_my_animal(); // Не указаны данные о животном!
--- Class_tasks/class_task_2.php
+++ Class_tasks/class_task_2.php
@@ -1,26 +1,55 @@
 <?php
 
-Class SquareChecker{
-    
-    public $part_list;
+class square_checker
+{
 
-    public function isSquare()
+    public array $part_list;
+
+    public function is_square(): string
     {
-        if (!isset($this->part_list)) {return 'Не указан массив со сторонами';}
-        if (count($this->part_list)!=4) {return 'Не корректное кол-во элементов фигуры';}
-
-        foreach ($this->part_list as $part){
-            if (!is_numeric($part)) { return 'Нужно вводить только числа';}
-            if ($part<0) {return 'Отрицательные цисла неприемлемы';}
+        if (!isset($this->part_list)) {
+            return 'Не указан массив со сторонами';
         }
-        
-        if (!(array_sum($this->part_list)/4==$this->part_list[0])){return 'Стороны квадрата должны быть равными';}
+        if (count($this->part_list) != 4) {
+            return 'Не корректное кол-во элементов фигуры';
+        }
 
-        return '<div style="width:'.($this->part_list[0]).'px;height:'.($this->part_list[0]).'px;border:1px solid;"></div>';
+        foreach ($this->part_list as $part) {
+            if (!is_numeric($part)) {
+                return 'Нужно вводить только числа';
+            }
+            if ($part < 0) {
+                return 'Отрицательные числа неприемлемы';
+            }
+            if ($this->part_list[0] !== $part) {
+                return 'Стороны квадрата должны быть равными';
+            }
+        }
+
+        return '<div style="width:' . ($this->part_list[0]) . 'px;height:' . ($this->part_list[0]) . 'px;border:1px solid;"></div>';
 
     }
 }
 
-$figure_1= new SquareChecker;
-$figure_1->part_list=[20,20,20,20];
-print $figure_1->isSquare();// Квадрат
+$figure_1 = new square_checker;
+$figure_1->part_list = [20, 20, 20, 20];
+print $figure_1->is_square(); // Квадрат
+
+$figure_2 = new square_checker;
+print $figure_2->is_square(); // 'Не указан массив со сторонами'
+
+$figure_3 = new square_checker;
+$figure_3->part_list = [20, 20, 20];
+print $figure_3->is_square(); // 'Не корректное кол-во элементов фигуры'
+
+$figure_4 = new square_checker;
+$figure_4->part_list = [20, 'Двадцать', 20, 20];
+print $figure_4->is_square(); // 'Нужно вводить только числа'
+
+$figure_5 = new square_checker;
+$figure_5->part_list = [20, 20, 20, -20];
+print $figure_5->is_square(); // 'Отрицательные числа неприемлемы'
+
+$figure_6 = new square_checker;
+$figure_6->part_list = [20, 20, 30, 20];
+print $figure_6->is_square(); // 'Стороны квадрата должны быть равными'
