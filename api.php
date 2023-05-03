d0cde111b14bd20b88741b695cf28befc0a03728
perfection <shark196@inbox.ru>
2023-05-03 18:15:11 +0300

    Добавлена улучшенная обработка ошибок api

--- api.php
+++ api.php
@@ -6,12 +6,19 @@ error_reporting(E_ALL);
 include_once 'get_db_info.php';
 header('Content-type: application/json');
 
-define ('ERROR_API_MESSAGES',[
-    'default_api_error'=> 'Некорректное обращение к api!',
-    'post_api_error'=>'Некорректное обращение методом POST к api!',
-    'get_api_error'=>'Некорректное обращение методом GET к api!',
-    'put_api_error'=>'Некорректное обращение методом PUT к api!',
-    'delete_api_error'=>'Некорректное обращение методом DELETE к api!'
+define('ERROR_API_MESSAGES', [
+    'default_api_error' => 'Некорректное обращение к api!',
+    'post_api_error' => 'Некорректное обращение методом POST к api!',
+    'get_api_error' => 'Некорректное обращение методом GET к api!',
+    'put_api_error' => 'Некорректное обращение методом PUT к api!',
+    'delete_api_error' => 'Некорректное обращение методом DELETE к api!'
+]);
+
+define('ALLOWED_REQUEST_METHODS', [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE'
 ]);
 
 /* http://mysite.local/api.php?path=applications GET*/
@@ -84,93 +91,116 @@ function manager_post_delete($manager_id): array
 
 function api()
 {
-    $result = [1];
-    $path = $_GET['path'] ?? '';
-    switch ($_SERVER['REQUEST_METHOD']) {
+    try {
+        if (!in_array($_SERVER['REQUEST_METHOD'], ALLOWED_REQUEST_METHODS)) {
+            throw new Exception(ERROR_API_MESSAGES['default_api_error']);
+        }
 
-        case 'GET':
-            switch ($path) {
-                case 'applications':
-                    if (!empty($_GET['id'])) {
-                        $result = applications_get_item($_GET['id']);
-                    } else {
-                        $result = applications_get_list();
-                    }
-                    ;
-                    break;
-                case 'managers':
-                    if (!empty($_GET['manager_id'])) {
-                        $result = manager_get_item($_GET['manager_id']);
-                    } else {
-                        $result = manager_get_list();
-                    }
-                    break;
-                default:
-                    $result=ERROR_API_MESSAGES['get_api_error'];
-                    break;   
-            }
-            break;
+        $result = [null];
+        $path = $_GET['path'] ?? '';
+        switch ($_SERVER['REQUEST_METHOD']) {
 
+            case 'GET':
+                switch ($path) {
+                    case 'applications':
+                        if (!empty($_GET['id'])) {
+                            $result = applications_get_item($_GET['id']);
+                        } else {
+                            $result = applications_get_list();
+                        }
+                        break;
 
-        case 'POST':
-            switch ($path) {
-                case 'applications':
-                    if (!empty($_POST)) {
-                        $result = application_post_create($_POST);
-                    }
-                    break;
-                case 'managers':
-                    if (!empty($_POST)) {
-                        $result = manager_post_create($_POST);
-                    }
-                    break;
-                default:
-                    $result=ERROR_API_MESSAGES['post_api_error'];
-                    break;    
-            }
-            break;
+                    case 'managers':
+                        if (!empty($_GET['manager_id'])) {
+                            $result = manager_get_item($_GET['manager_id']);
+                        } else {
+                            $result = manager_get_list();
+                        }
+                        break;
+
+                    default:
+                        throw new Exception(ERROR_API_MESSAGES['get_api_error']);
+
+                }
+                break;
 
 
-        case 'PUT':
-            switch ($path) {
-                case 'applications':
-                    if (!empty($_GET['id']) and !empty($_POST)) {
-                        $result = application_post_update($_GET['id'], $_POST);
-                    }
-                    break;
-                case 'managers':
-                    if (!empty($_GET['manager_id']) and !empty($_POST)) {
-                        $result = manager_post_update($_GET['manager_id'], $_POST);
-                    }
-                    break;
-                default:
-                    $result=ERROR_API_MESSAGES['put_api_error'];
-                    break;    
-            }   
-            break;
+            case 'POST':
+                switch ($path) {
+                    case 'applications':
+                        if (!empty($_POST)) {
+                            $result = application_post_create($_POST);
+                        } else {
+                            throw new Exception(ERROR_API_MESSAGES['post_api_error']);
+                        }
+                        break;
+
+                    case 'managers':
+                        if (!empty($_POST)) {
+                            $result = manager_post_create($_POST);
+                        } else {
+                            throw new Exception(ERROR_API_MESSAGES['post_api_error']);
+                        }
+                        break;
+
+                    default:
+                        throw new Exception(ERROR_API_MESSAGES['post_api_error']);
+
+                }
+                break;
 
 
-        case 'DELETE':
-            switch ($path) {
-                case 'applications':
-                    if (!empty($_GET['id'])) {
-                        $result = application_post_delete($_GET['id']);
-                    }
-                    break;
-                case 'managers':
-                    if (!empty($_GET['manager_id'])) {
-                        $result = manager_post_delete($_GET['manager_id']);
-                    }
-                    break;
-                default:
-                    $result=ERROR_API_MESSAGES['delete_api_error'];
-                    break;
-            }
-            break;     
-        
-        default:
-            $result=ERROR_API_MESSAGES['default_api_error'];
-            break;
+            case 'PUT':
+                switch ($path) {
+                    case 'applications':
+                        if (!empty($_GET['id']) and !empty($_POST)) {
+                            $result = application_post_update($_GET['id'], $_POST);
+                        } else {
+                            throw new Exception(ERROR_API_MESSAGES['put_api_error']);
+                        }
+                        break;
+
+                    case 'managers':
+                        if (!empty($_GET['manager_id']) and !empty($_POST)) {
+                            $result = manager_post_update($_GET['manager_id'], $_POST);
+                        } else {
+                            throw new Exception(ERROR_API_MESSAGES['put_api_error']);
+                        }
+                        break;
+
+                    default:
+                        throw new Exception(ERROR_API_MESSAGES['put_api_error']);
+
+                }
+                break;
+
+
+            case 'DELETE':
+                switch ($path) {
+                    case 'applications':
+                        if (!empty($_GET['id'])) {
+                            $result = application_post_delete($_GET['id']);
+                        } else {
+                            throw new Exception(ERROR_API_MESSAGES['delete_api_error']);
+                        }
+                        break;
+
+                    case 'managers':
+                        if (!empty($_GET['manager_id'])) {
+                            $result = manager_post_delete($_GET['manager_id']);
+                        } else {
+                            throw new Exception(ERROR_API_MESSAGES['delete_api_error']);
+                        }
+                        break;
+
+                    default:
+                        throw new Exception(ERROR_API_MESSAGES['delete_api_error']);
+
+                }
+                break;
+        }
+    } catch (Exception $error) {
+        $result = $error->getMessage();
     }
 
     print json_encode($result);
